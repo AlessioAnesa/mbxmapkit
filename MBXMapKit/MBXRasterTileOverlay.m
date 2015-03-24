8a290c2cee571a4c3348cbb5727ea7e6e50ae04f
Andrea Cremaschi <andreacremaschi@libero.it>
2015-03-24 10:29:48 +0100

    v4 API fix

--- MBXMapKit/MBXRasterTileOverlay.m
+++ MBXMapKit/MBXRasterTileOverlay.m
@@ -232,6 +232,10 @@
                                            dataName,
                                            (accessToken ? [@"?" stringByAppendingString:accessToken] : @"")]];
 
+    // Use larger tiles if on v4 API
+    //
+    if ([MBXMapKit accessToken]) self.tileSize = CGSizeMake(512, 512);
+
     // Default to covering up Apple's map
     //
     self.canReplaceMapContent = YES;
